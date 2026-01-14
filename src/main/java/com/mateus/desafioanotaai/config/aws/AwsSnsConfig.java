b0c33763f269b35a9cf28b92eeed844bcab6ce8a
Mateus Madeira <mafemad2005@gmail.com>
2026-01-14 15:38:13 -0300

    feat: setup conexão com aws e configuração topico sns

---
+++ src/main/java/com/mateus/desafioanotaai/config/aws/AwsSnsConfig.java
@@ -0,0 +1,38 @@
+package com.mateus.desafioanotaai.config.aws;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.Topic;
+
+@Configuration
+public class AwsSnsConfig {
+
+    @Value("${aws.region}")
+    private String region;
+    @Value("${aws.access-key-id}")
+    private String accessKeyId;
+    @Value("${aws.secret-access-key}")
+    private String secretAccessKey;
+    @Value("${aws.sns.topic-arn}")
+    private String catalogTopicArn;
+
+    @Bean
+    public SnsClient amazonSnsBuilder(){
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        return SnsClient.builder()
+                .credentialsProvider(() -> credentials)
+                .region(software.amazon.awssdk.regions.Region.of(region))
+                .build();
+    }
+
+    @Bean(name = "catalogEventsTopic")
+    public Topic snsCatalogTopic(){
+        return Topic.builder()
+                .topicArn(catalogTopicArn)
+                .build();
+    }
+
+}
